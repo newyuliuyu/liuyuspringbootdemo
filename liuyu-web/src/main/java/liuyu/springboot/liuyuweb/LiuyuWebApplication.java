0dce1f33ae007583d257ac9e48d1ddda53195f4b
liuyu <306487103@qq.com>
2018-06-22 15:27:44 +0800

    增加liuyu-web 用于练习web相关的内容

---
+++ liuyu-web/src/main/java/liuyu/springboot/liuyuweb/LiuyuWebApplication.java
@@ -0,0 +1,12 @@
+package liuyu.springboot.liuyuweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiuyuWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiuyuWebApplication.class, args);
+	}
+}
---
+++ liuyu-web/src/test/java/liuyu/springboot/liuyuweb/LiuyuWebApplicationTests.java
@@ -0,0 +1,16 @@
+package liuyu.springboot.liuyuweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LiuyuWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
